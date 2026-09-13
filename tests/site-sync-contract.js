ddce4d7b2e94cfedba6c6ba5a6115a99a976acb4
mohammadmk47 <mohammedmk3900@gmail.com>
2026-09-13 23:49:50 +0330

    test: enforce shared brand mark on every page

--- tests/site-sync-contract.js
+++ tests/site-sync-contract.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
 
-const index = fs.readFileSync('site/index.html', 'utf8');
-const developers = fs.readFileSync('site/developers.html', 'utf8');
-const downloads = fs.readFileSync('site/downloads.html', 'utf8');
+const pages = [
+  'site/index.html',
+  'site/downloads.html',
+  'site/developers.html',
+  'site/about.html',
+  'site/faq.html',
+  'site/404.html'
+];
+
+const sources = pages.map((file) => [file, fs.readFileSync(file, 'utf8')]);
+
+const index = sources.find(([file]) => file === 'site/index.html')[1];
+const developers = sources.find(([file]) => file === 'site/developers.html')[1];
+const downloads = sources.find(([file]) => file === 'site/downloads.html')[1];
 
 const required = [
   ['site/index.html', index, 'docs/build.md'],
@@ -14,6 +25,12 @@ const required = [
 
 for (const [file, source, needle] of required) {
   if (!source.includes(needle)) throw new Error(`${file}: missing current VoidOne sync contract ${needle}`);
+}
+
+for (const [file, source] of sources) {
+  if (!source.includes('assets/icons/voidone-mark.svg')) {
+    throw new Error(`${file}: missing shared VoidOne brand mark`);
+  }
 }
 
 if (downloads.includes('MSI Package') || downloads.includes('data-download="msi"')) {
